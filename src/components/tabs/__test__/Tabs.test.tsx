ae3ad7d01e510e8dc856fda92f7251a9a755e9d0
吴政衍 <160201@nd.com>
2018-12-12 09:24:25 +0800

    update: tabs add unit-test

--- src/components/tabs/__test__/Tabs.test.tsx
+++ src/components/tabs/__test__/Tabs.test.tsx
@@ -1,4 +1,4 @@
-import { shallow, configure } from 'enzyme';
+import { mount, shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react'
 import Tabs from '../index'
@@ -11,7 +11,7 @@ describe('Tabs tests', () => {
     const staticProperty = Tabs.TabPane
     expect(staticProperty).toBe(TabPane)
   })
-  test('render three tabPanes', () => {
+  test('render three tabBar Items', () => {
     const wrapper = shallow(
       <Tabs>
         <TabPane tab="tab 1" key="1">content 1</TabPane>
@@ -20,5 +20,38 @@ describe('Tabs tests', () => {
       </Tabs>
     )
     expect(wrapper.find('.rfox-tabs__nav-link').length).toBe(3)
+  })
+  test('render one tabContent', () => {
+    const wrapper = shallow(
+      <Tabs>
+        <TabPane tab="tab 1" key="1">content 1</TabPane>
+        <TabPane tab="tab 2" key="2">content 2</TabPane>
+        <TabPane tab="tab 3" key="3">content 3</TabPane>
+      </Tabs>
+    )
+    expect(wrapper.find(TabPane).length).toBe(1)
+  })
+})
+
+describe('TabPane tests', () => {
+  test('check props', () => {
+    const wrapper = shallow(
+      <Tabs>
+        <TabPane tab="tab 1" key="1">content 1</TabPane>
+      </Tabs>
+    )
+    expect(wrapper.find(TabPane).prop('tab')).toBe('tab 1')
+    expect(wrapper.find(TabPane).key()).toBe('1')
+  })
+  test('render children correctly', () => {
+    const textWrapper = shallow(
+      <TabPane tab="tab 1" key="1">content 1</TabPane>
+    )
+    const MockComponent = () => <div></div>
+    const componentWrapper = shallow(
+      <TabPane tab="tab 1" key="1"><MockComponent /></TabPane>
+    )
+    expect(textWrapper.children().text()).toBe('content 1')
+    expect(componentWrapper.find(MockComponent).exists()).toBe(true)
   })
 })
