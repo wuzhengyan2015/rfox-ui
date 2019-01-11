9be783fb657c635b6615c4db15f582125735c283
吴政衍 <160201@nd.com>
2019-01-11 09:43:16 +0800

    update: add unit test for rate

--- src/components/rate/__tests__/index.test.tsx
+++ src/components/rate/__tests__/index.test.tsx
@@ -1,10 +1,32 @@
-import { render, configure } from 'enzyme'
+import { mount, shallow, render, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
+import Rate from '../index'
 
 configure({ adapter: new Adapter() })
 
+const mockRate = <Rate />
+
 describe('Rate tests', () => { 
     it('renders correctly', () => {
+        const tree = render(
+            <Rate />
+        )
+        expect(tree).toMatchSnapshot()
+    })
+    it('default count prop test', () => {
+        const wrapper = shallow(mockRate)
+        expect(wrapper.find('.rfox-rate_item').length).toBe(5)
+    })
+    it('count prop test', () => {
+        const wrapper = shallow(<Rate count={10} />)
+        expect(wrapper.find('.rfox-rate_item').length).toBe(10)
+    })
+    it('allClear prop test', () => {
+        const wrapper = mount(<Rate allClear/>)
+        wrapper.find('.rfox-rate_item').at(2).simulate('click')
+        expect(wrapper.find('.rfox-rate_item--hover').length).toBe(3)
+        wrapper.find('.rfox-rate_item').at(2).simulate('click')
+        expect(wrapper.find('.rfox-rate_item--hover').length).toBe(0)
     })
 })
