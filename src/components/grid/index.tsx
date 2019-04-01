67fa1b7c15e289c2df44b7f46e6bee21c86465ae
吴政衍 <160201@nd.com>
2019-04-01 09:30:07 +0800

    add grid interface

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { SFC } from 'react'
 
-const Row = () => {}
+interface IBaseColProps {
+    offset?: number;
+    order?: number;
+    pull?: number;
+    push?: number;
+    span?: number;
+}
 
-const Col = () => {}
+export interface IRowProps {
+    align?: string; // top middle bottom
+    gutter?: number | {
+        xs?: number;
+        sm?: number;
+        md?: number;
+        lg?: number;
+        xl?: number;
+        xxl?: number;
+    }
+    justify?: string;
+    type?: string;
+}
+
+export interface IColProps extends IBaseColProps {
+    xs?: number | IBaseColProps;
+    sm?: number | IBaseColProps;
+    md?: number | IBaseColProps;
+    lg?: number | IBaseColProps;
+    xl?: number | IBaseColProps;
+    xxl?: number | IBaseColProps;
+}
+
+const Row: SFC<IRowProps> = () => {
+    return <div></div>
+}
+
+const Col: SFC<IColProps> = () => {
+    return <div></div>
+}
 
 const Grid = { Row, Col }
 
