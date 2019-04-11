267e9a2461b5ee6fdbd81f7e72f063dd25da2c29
吴政衍 <160201@nd.com>
2019-04-11 08:58:24 +0800

    add basic layout

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,11 @@
+import React, { SFC } from 'react'
+import cx from 'classnames'
+import './styles/index.scss'
+
+const Layout:SFC = () => {
+    return (
+        <section className="ant-layout"></section>
+    )
+}
+
+export default Layout
