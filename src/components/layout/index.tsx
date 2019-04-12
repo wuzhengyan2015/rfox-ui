f25f613061ea71ae0146e22fa0981dd4cf63afdf
吴政衍 <160201@nd.com>
2019-04-12 09:40:43 +0800

    update layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,96 @@
-import React, { SFC } from 'react'
+import React, { FunctionComponent, CSSProperties } from 'react'
 import cx from 'classnames'
 import './styles/index.scss'
 
-const Layout:SFC = () => {
+interface ILayoutProps {
+    className?: string;
+    style?: CSSProperties;
+    hasSider?: boolean;
+}
+
+const Layout: FunctionComponent<ILayoutProps> & {
+    Header: FunctionComponent<ILayoutProps>;
+    Content: FunctionComponent<ILayoutProps>;
+    Footer: FunctionComponent<ILayoutProps>;
+    Sider: FunctionComponent<ILayoutProps>;
+  } = (props) => {
+    const { className, style, hasSider, children } = props
     return (
-        <section className="ant-layout"></section>
+        <section
+            className={ cx('ant-layout', {
+                [className]: !!className,
+                'ant-layout-has-sider': !!hasSider
+            }) }
+            style={style}
+        >
+            { children }
+        </section>
     )
 }
+
+const Header: FunctionComponent<ILayoutProps> = (props) => {
+    const { className, style, hasSider, children } = props
+    return (
+        <header
+            className={ cx('ant-layout-header', {
+                [className]: !!className,
+                'ant-layout-has-sider': !!hasSider
+            }) }
+            style={style}
+        >
+            { children }
+        </header>
+    )
+}
+
+const Content: FunctionComponent<ILayoutProps> = (props) => {
+    const { className, style, hasSider, children } = props
+    return (
+        <main
+            className={ cx('ant-layout-content', {
+                [className]: !!className,
+                'ant-layout-has-sider': !!hasSider
+            }) }
+            style={style}
+        >
+            { children }
+        </main>
+    )
+}
+
+const Footer: FunctionComponent<ILayoutProps> = (props) => {
+    const { className, style, hasSider, children } = props
+    return (
+        <footer
+            className={ cx('ant-layout-footer', {
+                [className]: !!className,
+                'ant-layout-has-sider': !!hasSider
+            }) }
+            style={style}
+        >
+            { children }
+        </footer>
+    )
+}
+
+const Sider: FunctionComponent<ILayoutProps> = (props) => {
+    const { className, style, hasSider, children } = props
+    return (
+        <aside
+            className={ cx('ant-layout-sider', {
+                [className]: !!className,
+                'ant-layout-has-sider': !!hasSider
+            }) }
+            style={style}
+        >
+            { children }
+        </aside>
+    )
+}
+
+Layout.Header = Header
+Layout.Content = Content
+Layout.Footer = Footer
+Layout.Sider = Sider
 
 export default Layout
--- src/demo.tsx
+++ src/demo.tsx
@@ -4,12 +4,47 @@ import './assets/styles/reset.css'
 import './assets/styles/global.css'
 import { ModalMethod, NormalModal, UncontrollerModal } from './components/modal/demo'
 import { NormalGrid, FlexGrid, ResponsiveGrid } from './components/grid/demo'
+import Layout from './components/layout'
+
+const { Header, Content, Footer, Sider } = Layout
 
 
 ReactDOM.render(
     <div>
         {/* <NormalModal /> */}
         { <NormalGrid/> }
+        <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+
+        <Layout>
+          <Header>Header</Header>
+          <Layout>
+            <Sider>Sider</Sider>
+            <Content>Content</Content>
+          </Layout>
+          <Footer>Footer</Footer>
+        </Layout>
+
+        <Layout>
+          <Header>Header</Header>
+          <Layout>
+            <Content>Content</Content>
+            <Sider>Sider</Sider>
+          </Layout>
+          <Footer>Footer</Footer>
+        </Layout>
+
+        <Layout>
+          <Sider>Sider</Sider>
+          <Layout>
+            <Header>Header</Header>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+          </Layout>
+        </Layout>
     </div>,
     document.getElementById('root')
 )
